41be51b64663d4d46a8b7c143a2183a1fb0fd74a
Blake McLeod <lbmcleod@gmail.com>
2014-02-19 01:24:33 -0500

    Added angular google map plugin, set up plotting points to map

--- public/js/controllers/map.js
+++ public/js/controllers/map.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var myApp = angular.module('MapPlotter', ['jsonService']);
+var myApp = angular.module('MapPlotter', ['jsonService', 'google-maps']);
 
 myApp.controller('MapCtrl', ['$scope', function ($scope) {
 
@@ -9,7 +9,29 @@ myApp.controller('MapCtrl', ['$scope', function ($scope) {
 	]);
 
 myApp.controller('PlotsCtrl', function ($scope, JsonService) {
+	$scope.markerList = [];
 	JsonService.get(function (data) {
-		$scope.locations = data.locations;
+		$scope.locations = data.locations.splice(0, 10000);
+		angular.forEach($scope.locations, function (value) {
+			var temp = {
+				latitude : [value.latitude.toString().slice(0, 2), '.', value.latitude.toString().slice(2)].join(''),
+				longitude : [value.longitude.toString().slice(0, 3), '.', value.longitude.toString().slice(3)].join(''),
+				showWindow : true,
+			};
+			this.push(temp);
+		}, $scope.markerList);
 	});
+
+	$scope.mapPoint = function (x, y) {
+		console.log(x + ', ' + y);
+	};
+
+	$scope.map = {
+		center : {
+			latitude : 29,
+			longitude : -82
+		},
+		markers : $scope.markerList,
+		zoom : 8
+	};
 });
